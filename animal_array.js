fbca4f45d90e8fca372117bede7e41d119365636
Corey M <63071835+coreymichaud@users.noreply.github.com>
2020-09-26 15:17:01 -0400

    Add files via upload

---
+++ animal_array.js
@@ -0,0 +1,69 @@
+var randAni = [
+
+    /* DISCLAIMER: If anyone's looking at this, please don't be mad if I misplace where an animal lives :( For some, I placed them where they mostly live or if I don't
+    specifically have a category for them. */
+
+    //African Animals
+    'https://en.wikipedia.org/wiki/Giraffe', 'https://en.wikipedia.org/wiki/Lion', 'https://en.wikipedia.org/wiki/Cheetah',
+    'https://en.wikipedia.org/wiki/Zebra', 'https://en.wikipedia.org/wiki/Manatee', 'https://en.wikipedia.org/wiki/African_elephant',
+
+    //Asian Animals
+    'https://en.wikipedia.org/wiki/Asian_elephant', 'https://en.wikipedia.org/wiki/Giant_panda', 'https://en.wikipedia.org/wiki/Sunda_flying_lemur',
+    'https://en.wikipedia.org/wiki/Asian_black_bear', 'https://en.wikipedia.org/wiki/Red_panda', 'https://en.wikipedia.org/wiki/Bengal_tiger',
+    'https://en.wikipedia.org/wiki/Yellow-throated_marten',
+
+    //Australian Animals
+    'https://en.wikipedia.org/wiki/Koala', 'https://en.wikipedia.org/wiki/Irrawaddy_dolphin', 'https://en.wikipedia.org/wiki/Sugar_glider',
+    'https://en.wikipedia.org/wiki/Quokka', 
+
+    //Arctic Animals
+    'https://en.wikipedia.org/wiki/Emperor_penguin', 'https://en.wikipedia.org/wiki/Beluga_whale', 
+
+    //North American Animals
+    'https://en.wikipedia.org/wiki/Raccoon', 'https://en.wikipedia.org/wiki/Grizzly_bear', 'https://en.wikipedia.org/wiki/Goose',
+    'https://en.wikipedia.org/wiki/Red_fox', 'https://en.wikipedia.org/wiki/American_bison', 'https://en.wikipedia.org/wiki/Opossum',
+
+    //South American Animals
+    'https://en.wikipedia.org/wiki/Jaguar', 'https://en.wikipedia.org/wiki/Two-toed_sloth', 'https://en.wikipedia.org/wiki/Three-toed_sloth',
+
+    //Literally Everywhere Animals
+    'https://en.wikipedia.org/wiki/Honey_bee', 'https://en.wikipedia.org/wiki/Horse', 'https://en.wikipedia.org/wiki/Lynx',
+    'https://en.wikipedia.org/wiki/Highland_cattle', 'https://en.wikipedia.org/wiki/Texas_Longhorn', 'https://en.wikipedia.org/wiki/Rabbit',
+
+    //Dogs
+    'https://en.wikipedia.org/wiki/German_Shepherd', 'https://en.wikipedia.org/wiki/Pomeranian_(dog)', 'https://en.wikipedia.org/wiki/Welsh_Corgi',
+    'https://en.wikipedia.org/wiki/Golden_Retriever', 'https://en.wikipedia.org/wiki/Portuguese_Water_Dog', 'https://en.wikipedia.org/wiki/Husky',
+    'https://en.wikipedia.org/wiki/Pit_bull',
+
+    //Domesticated Cats
+    'https://en.wikipedia.org/wiki/Siberian_cat', 'https://en.wikipedia.org/wiki/Pallas%27s_cat',
+
+    //Sharks
+    'https://en.wikipedia.org/wiki/Goblin_shark', 'https://en.wikipedia.org/wiki/Cookiecutter_shark', 'https://en.wikipedia.org/wiki/Great_white_shark',
+    'https://en.wikipedia.org/wiki/Whale_shark',
+
+    //Marine Animals
+    'https://en.wikipedia.org/wiki/Lamprey', 'https://en.wikipedia.org/wiki/Sea_turtle', 'https://en.wikipedia.org/wiki/Stingray',
+    'https://en.wikipedia.org/wiki/North_American_river_otter', 'https://en.wikipedia.org/wiki/Hermit_crab', 'https://en.wikipedia.org/wiki/Killer_whale',
+    'https://en.wikipedia.org/wiki/Narwhal', 'https://en.wikipedia.org/wiki/Harp_seal',
+
+    //Dinosaurs (Yes I know dinosaurs aren't animals)
+    'https://en.wikipedia.org/wiki/Brontosaurus',
+
+    //Primates
+    'https://en.wikipedia.org/wiki/Mandrill', 'https://en.wikipedia.org/wiki/Japanese_macaque', 'https://en.wikipedia.org/wiki/Mountain_gorilla',
+    'https://en.wikipedia.org/wiki/Tarsier',
+
+    //Birds
+    'https://en.wikipedia.org/wiki/Hummingbird', 'https://en.wikipedia.org/wiki/Kingfisher', 'https://en.wikipedia.org/wiki/Great_tit',
+    'https://en.wikipedia.org/wiki/Bufflehead',
+
+    //Amphibians
+    'https://en.wikipedia.org/wiki/Desert_rain_frog', 'https://en.wikipedia.org/wiki/True_frog',    
+
+    //Reptiles
+    'https://en.wikipedia.org/wiki/Alligator_snapping_turtle', 'https://en.wikipedia.org/wiki/Spectral_pygmy_chameleon', 'https://en.wikipedia.org/wiki/Veiled_chameleon',
+    'https://en.wikipedia.org/wiki/Jackson%27s_chameleon', 'https://en.wikipedia.org/wiki/Blue-tongued_skink', 'https://en.wikipedia.org/wiki/American_alligator',
+    'https://en.wikipedia.org/wiki/Yellow-bellied_slider',
+
+]
